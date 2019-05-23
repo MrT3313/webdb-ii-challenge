41e3c8f86a233373948dcf2ad8446bb0f50b23f4
MrT3313 <rturgeon13@gmail.com>
2019-05-22 20:48:12 -0500

    preclass update

---
+++ api/kinex.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    development: {
+    client: 'sqlite3',
+    connection: {
+        filename: './data/budget.sqlite3'
+    },
+    useNullAsDefault: true
+    },
+};
--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const server = express();
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// START ENDPOINTS
+
+
+
+
+
+// END ENDPOINTS
 
 const port = 3300;
 server.listen(port, function() {
