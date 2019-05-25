a56e72e0c4f5f3f2ad6720dfea83f1024d3b8c66
MrT3313 <rturgeon13@gmail.com>
2019-05-25 12:53:19 -0500

    MVP

--- api/zooRouter.js
+++ api/zooRouter.js
@@ -23,54 +23,89 @@ const knex_DBversion = require('../data/dbConfig')
                 res.status(500).json({ error: "could not GET/ animals"})
             })
     })
+
+    router.get("/:id", async (req,res) => {
+        console.log("zooRouter GET/:id")
+        const { id } = req.params
+        
+        knex_DBversion('zoos')
+            .where({ id })
+                .then( animal => {
+                    console.log(animal)
+                    res.status(200).json(animal)
+                })
+                .catch( () => {
+                    res.status(500).json({ error: "could not GET/:id individual animal"})
+                })
+    })
 // - POST - //
+    // ACCEPTED SHAPE
+        // {
+        //     "name": "UPDATED TACO"
+        // }
     router.post("/", async (req, res) => {
         console.log("zooRouter POST/")
         knex_DBversion('zoos')
             .insert(req.body)
                 .then(results => 
-                    res.status(200).json(results))
+                    res.status(201).json(results))
                 .catch( () => {
                     res.status(500).json({ error: "could not post new animal"})
                 })
     })
 
 // - PUT - *** *** *** NOT WORKING *** *** *** //
+    // ACCEPTED SHAPE
+        //
+        //
+        //
+
     router.put("/:id", async (req, res) => {
         console.log("zooRouter PUT/:id")
         const { id } = req.params
         
-        console.log(req.body)
-        console.log(id)
-
-        const updateObject = {
-            "id": id,
-            "name": req.body.name
-        }
-        console.log(updateObject)
+        
+        // V1
+            console.log(req.body)
+            console.log(id)
+        //V2
+            const updateObject = {
+                "id": id,
+                "name": req.body.name
+            }
+            console.log(updateObject)
 
         knex_DBversion('zoos')
-            // .update(updateObject)
+            .where({ id })
+            
+            //V1
+                // .update(id, req.body)
 
-            // .update({
-            //     "id": id, 
-            //     "name": req.body
-            // })
+            // V2
+                // .update(updateObject)
 
-            // .update(id, req.body)
+            // V3
+                // .update({
+                //     "id": id, 
+                //     "name": req.body
+                // })
+            
+            // V4
+                .update(req.body)
 
-                .then( results => {
-                    console.log(results)
 
-                    if (results) {
-                        res.status(200).json(results)
-                    } else {
-                        res.status(404).json({ message: "animal not found"})
-                    }
-                })
-                .catch( () => {
-                    res.status(500).json({ error: "could not update animal"})
-                })
+            .then( results => {
+                console.log(results)
+
+                if (results) {
+                    res.status(200).json(results)
+                } else {
+                    res.status(404).json({ message: "animal not found"})
+                }
+            })
+            .catch( () => {
+                res.status(500).json({ error: "could not update animal"})
+            })
     } )
 
 // - DELETE - //
@@ -87,7 +122,6 @@ const knex_DBversion = require('../data/dbConfig')
                     } else {
                         res.status(404).json({ error: "animal not found"})
                     }
-                    
                 })
                 .catch( () => {
                     res.status(500).json({error: "unable to delete animal"})
